e70cedfd8b6d251d2cdf0afe229a2c9ed72cec3e
maksitox <max.poliuha@gmail.com>
2024-09-16 17:11:21 +0300

    0.0.2 Added Input Field - User Interaction

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,44 @@ class MainApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Crypto Wallet Test App'),
         ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: CryptoInputField(),
+      ),
+    );
+  }
+}
+
+class CryptoInputField extends StatefulWidget {
+  const CryptoInputField({super.key});
+
+  @override
+  _CryptoInputFieldState createState() => _CryptoInputFieldState();
+}
+
+class _CryptoInputFieldState extends State<CryptoInputField> {
+  String _cryptoName = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text('Enter Cryptocurrency Name:'),
+          TextField(
+            onChanged: (value) {
+              setState(() {
+                _cryptoName = value;
+              });
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Cryptocurrency',
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text('You entered: $_cryptoName'),
+        ],
       ),
     );
   }
